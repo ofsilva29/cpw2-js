18f3427b424378feb0c7c5b09d07f190ca032dd5
ofsilva29 <112443937+ofsilva29@users.noreply.github.com>
2023-03-31 18:20:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,24 @@
  * @returns Um array com os nomes invertidos
  */
 function invert(people){
-
+    let people = [];
+    let arrayLength = prompt ("Nomes");
+    
     // TODO
     // 1) Peça ao usuário para digitar vários nomes. Exiba na tela
     // todos os nomes digitados, porém de maneira invertida (do último para o primeiro).
     // Uma dica, você pode utilizar a função prompt para permitir que o usuário digite os
     // nomes dos usuários.
-
+    
+    for (let i = 0; i< arrayLength; i++ ) {
+        people [i] = prompt ("Digitar nome dos usuários :");
+    }
+    
     return [];
-}
+    
+    people = people.reverse ();
+    return people;
+    }
 
 /**
  * Essa função recebe um array de notas e retorna uma média
@@ -22,16 +31,28 @@ function invert(people){
  * @param {*} grades : Um array de notas
  * @returns Uma média a partir do array de notas
  */
-function mean(grades){
-
+function mean (grades){
+    let name = prompt ("Digite o nome do aluno :");
+    let grades = [];
+    let mean = 0 ;
+    
+    for (let i = 0; i < 3; i++0 {
+        grades [i] = prompt ("Digite o nome do aluno :");
+        mean += number (grades{i});
+    }
+  
     // TODO
     // 2) Faça um programa que leia o nome e as três notas de uma disciplina de um aluno e ao final escreva
     // o nome do aluno, sua média e se ele foi aprovado, sabendo-se que a média para aprovação é igual
     // ou superior a 7.
 
+    mean /= 3;
+    
     return 0;
-}
+    return mean;
 
+}
+   
 /**
  * Função recebe uma média e informa textualmente se um estudante está aprovado ou não
  *
@@ -46,6 +67,14 @@ function isApproved(mean){
     // ou superior a 7.
 
     return "";
+    
+    if (mean >= 7) {
+        return "Aprovado"; 
+    } else {
+        return "Reprovado";
+    
+    }
+
 }
 
 /**
@@ -55,6 +84,11 @@ function isApproved(mean){
  * @returns Uma descrição da data informada
  */
 function wide(strDate){
+    
+    let strDate = prompt ("Digite uma data (dd/mm/aaaa):").split("/");
+    let mesArray = ["", "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+    
+}
 
     // TODO
     // 3) Faça um script que receba uma data no formato “dd/mm/aaaa” e escreva a data por extenso,
@@ -63,6 +97,11 @@ function wide(strDate){
     // Nesse caso, o separador é a barra (/) da data.
 
     return "";
+    if (strDate[0] < 1 || strDate[0] > 31 || strDate[1] < 1 || strDate[1] > 12){
+        return "";
+    } else {
+        return strDate[0] + " de " + mesArray[Number(strDate[1])] + " de " + strDate[2];
+    }
 }
 
 // Trecho utilizado pelos testes
